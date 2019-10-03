98ccaaae4f13dae8c37ae090aee6e0c97402ee32
Ania-chan <ania.jurago@gmail.com>
2019-10-03 21:06:07 +0900

    refactored vuetify.js file

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,14 +1,24 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify';
+import Vuetify from 'vuetify/lib';
+import 'vuetify/dist/vuetify.min.css'
 
-Vue.use(Vuetify, {
-  theme: {
-    primary: '#47b784',
-    accent: '#e76f54',
-    secondary: '#34495e',
-    info: '#0D47A1',
-    warning: '#ffb300',
-    error: '#B71C1C',
-    success: '#2E7D32',
-  },
-});
+Vue.use(Vuetify)
+
+export default ctx => {
+  const vuetify = new Vuetify({
+    theme: {
+      themes: {
+        primary: '#47b784',
+        accent: '#e76f54',
+        secondary: '#34495e',
+        info: '#0D47A1',
+        warning: '#ffb300',
+        error: '#B71C1C',
+        success: '#2E7D32',
+      }
+    },
+  })
+
+  ctx.app.vuetify = vuetify
+  ctx.$vuetify = vuetify.framework
+}
