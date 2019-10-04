ca4e938df7a83463a9370e9a0df4819d20b367ba
Ania-chan <ania.jurago@gmail.com>
2019-10-05 00:03:57 +0900

    renamed flat to text

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,22 +1,22 @@
 <template>
   <v-footer height="auto" color="secondary darken-2">
     <v-layout class="footer-links hidden-sm-and-down" justify-start align-center row wrap>
-      <v-btn color="white" flat round to="/credits">{{ $t("credits") }}</v-btn>
+      <v-btn color="white" text round to="/credits">{{ $t("credits") }}</v-btn>
       <v-btn
         color="white"
-        flat
+        text
         round
         href="https://spark.adobe.com/page/FFczmvHR6Brpu/"
         target="_blank"
       >{{ $t("press") }}</v-btn>
       <v-btn
         color="white"
-        flat
+        text
         round
         href="mailto:info@vuevixens.org"
         target="_blank"
       >{{ $t("contact") }}</v-btn>
-      <v-btn color="white" flat round to="/about">{{ $t("charity") }}</v-btn>
+      <v-btn color="white" text round to="/about">{{ $t("charity") }}</v-btn>
     </v-layout>
     <v-layout justify-end align-center row wrap>
       <a href="https://tinyletter.com/VueVixens" target="_blank">
