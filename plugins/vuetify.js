a6c491f54ac5618d22e9c1bd3b6c531b5e7cce5b
Ania-chan <ania.jurago@gmail.com>
2019-10-04 23:50:34 +0900

    installed @mdi/font and refactored vuetify.js

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
-import 'vuetify/src/styles/main.sass'
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css'
+import '@mdi/font/css/materialdesignicons.css'
 
-Vue.use(Vuetify, {
-  theme: {
-    themes: {
+Vue.use(Vuetify);
+export default ctx => {
+  const vuetify = new Vuetify({
+    theme: {
+      dark: false, // From 2.0 You have to select the theme dark or light here
       primary: '#47b784',
       accent: '#e76f54',
       secondary: '#34495e',
@@ -13,5 +16,7 @@ Vue.use(Vuetify, {
       error: '#B71C1C',
       success: '#2E7D32',
     }
-  },
-})
+  })
+  ctx.app.vuetify = vuetify
+  ctx.$vuetify = vuetify.framework
+}
