10e914dd2aa0f03d4402762b422606afd292b69a
Jen Looper <jen.looper@gmail.com>
2019-11-29 23:06:07 -0500

    Repairing dev branch Drawer

--- components/Drawer.vue
+++ components/Drawer.vue
@@ -1,43 +1,43 @@
 <template>
-  <v-navigation-drawer
-    disable-resize-watcher
-    :value="showDrawer"
-    @input="$emit('toggleDrawer', $event)"
-    app
-    class="secondary darken-2"
-    dark
-  >
-    <v-list>
-      <v-list-item to="/about">
-        <v-list-item-title class="subheading">{{ $t("menu.about") }}</v-list-item-title>
-      </v-list-item>
-      <v-list-item to="/sponsors">
-        <v-list-item-title class="subheading">{{ $t("menu.sponsors") }}</v-list-item-title>
-      </v-list-item>
-      <v-list-item to="/team">
-        <v-list-item-title class="subheading">{{ $t("menu.team") }}</v-list-item-title>
-      </v-list-item>
-      <v-list-item to="/gallery">
-        <v-list-item-title class="subheading">{{ $t("menu.gallery") }}</v-list-item-title>
-      </v-list-item>
+	<v-navigation-drawer
+		disable-resize-watcher
+		:value="showDrawer"
+		@input="$emit('toggleDrawer', $event)"
+		app
+		class="secondary darken-2"
+		dark
+	>
+		<v-list>
+			<v-list-tile to="/about">
+				<v-list-tile-title class="subheading">{{ $t('menu.about') }}</v-list-tile-title>
+			</v-list-tile>
+			<v-list-tile to="/sponsors">
+				<v-list-tile-title class="subheading">{{ $t('menu.sponsors') }}</v-list-tile-title>
+			</v-list-tile>
+			<v-list-tile to="/team">
+				<v-list-tile-title class="subheading">{{ $t('menu.team') }}</v-list-tile-title>
+			</v-list-tile>
+			<v-list-tile to="/gallery">
+				<v-list-tile-title class="subheading">{{ $t('menu.gallery') }}</v-list-tile-title>
+			</v-list-tile>
 
-      <v-list-tile href="https://dev.to/vuevixens" target="_blank">
-        <v-list-tile-title class="subheading">Blog</v-list-tile-title>
-      </v-list-tile>
-      <v-list-tile href="https://etsy.com/shop/vuevixens" target="_blank">
-        <v-list-tile-title class="subheading">{{ $t("menu.shop") }}</v-list-tile-title>
-      </v-list-tile>
-    </v-list>
-  </v-navigation-drawer>
+			<v-list-tile href="https://dev.to/vuevixens" target="_blank">
+				<v-list-tile-title class="subheading">Blog</v-list-tile-title>
+			</v-list-tile>
+			<v-list-tile href="https://etsy.com/shop/vuevixens" target="_blank">
+				<v-list-tile-title class="subheading">{{ $t('menu.shop') }}</v-list-tile-title>
+			</v-list-tile>
+		</v-list>
+	</v-navigation-drawer>
 </template>
 
 <script>
-import messages from "../assets/translations/header";
+import messages from '../assets/translations/header';
 
 export default {
-  props: ["showDrawer"],
-  i18n: {
-    messages
-  }
+	props: ['showDrawer'],
+	i18n: {
+		messages,
+	},
 };
 </script>
