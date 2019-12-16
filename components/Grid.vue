5a03566c54d70ce0f7399246d743826c96267ed2
Jen Looper <jen.looper@gmail.com>
2019-12-16 18:27:46 -0500

    Bugfix: we need to link directly from the homepage for big events

--- components/Grid.vue
+++ components/Grid.vue
@@ -2,7 +2,27 @@
   <v-container grid-list-lg>
     <v-layout wrap>
       <v-flex xs12 sm4 md3 v-for="(event, index) in events" :key="`${event.name}-${index}`">
-        <v-card class="vv-card" hover :to="event.link.cached_url" nuxt>
+        <v-card
+          v-if="event.link.linktype == 'cached_url'"
+          class="vv-card"
+          hover
+          :to="event.link.cached_url"
+          nuxt
+        >
+          <client-only>
+            <v-img class="white--text vv-card-image grey lighten-2" height="200px" :src="event.img">
+              <v-layout fill-height align-center justify-center ma-0>
+                <v-flex xs12 d-flex align-center justify-center class="event-name">
+                  <div>
+                    <h3 class="headline">{{event.location}}</h3>
+                    <p class="title">{{event.date | moment('MMM Do')}}</p>
+                  </div>
+                </v-flex>
+              </v-layout>
+            </v-img>
+          </client-only>
+        </v-card>
+        <v-card v-else class="vv-card" hover :href="event.link.cached_url" nuxt>
           <client-only>
             <v-img class="white--text vv-card-image grey lighten-2" height="200px" :src="event.img">
               <v-layout fill-height align-center justify-center ma-0>
